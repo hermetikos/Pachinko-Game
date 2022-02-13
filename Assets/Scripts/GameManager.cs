f19545c5ad7fee9a56bdbfa4b225e48e62736597
Stan S <sslup@hermetikos.com>
2022-02-12 23:59:14 -0500

    Implements game over and restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
-    [SerializeField] private TextMeshProUGUI ballCountUI;
-    [SerializeField] private int ballCount = 20;
-    private int ballsInNests = 0;
+    
+    public delegate void GameEvent();
+    public static event GameEvent GameOver;
 
+    [SerializeField] private TextMeshProUGUI ballCountUI;
+    [SerializeField] private GameObject GameOverScreen;
+    [SerializeField] private int ballCount = 20;
+    private bool gameOver = false;
     private void OnEnable() {
         Counter.UpdateBallCount += this.UpdateBallCount;
+        PlayerController.UpdateBallCount += this.UpdateBallCount;
     }
+
+    private void OnDisable() {
+        Counter.UpdateBallCount -= this.UpdateBallCount;
+        PlayerController.UpdateBallCount -= this.UpdateBallCount;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        // update the UI        
+        if (ballCount <= 0
+        && GameOver != null)
+        {
+            GameOver();
+            GameOverScreen.SetActive(true);
+        }
     }
 
     
@@ -25,5 +42,10 @@ public class GameManager : MonoBehaviour
         ballCount += addend;
         ballCountUI.SetText("Ball Count: " + ballCount);
         Debug.Log("Ball Count: " + ballCount);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using System;
 
 public class PlayerController : MonoBehaviour
 {
+    public delegate void ScoreAction(int ballCount);
+    public static event ScoreAction UpdateBallCount;
+
     [SerializeField] private float maxHoldTime = 2.0f;
     
     [SerializeField] private float maxLaunchForce = 60.0f;
@@ -13,7 +16,16 @@ public class PlayerController : MonoBehaviour
     
     [SerializeField] private GameObject ballSpawn;
     [SerializeField] private LaunchArea launchArea;
+
     private System.DateTime currentTime;
+    private bool gameOver = false;
+
+    private void OnEnable() {
+        GameManager.GameOver += this.GameOver;
+    }
+    private void OnDisable() {
+        GameManager.GameOver -= this.GameOver;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -31,10 +43,13 @@ public class PlayerController : MonoBehaviour
         // if(currentTime.Millisecond > 0.0f && Input.GetKeyUp(KeyCode.LeftControl)) {
         //     SpawnBall();            
         // }
-        if(Input.GetKeyDown(KeyCode.LeftControl)
-            && launchArea.isLaunchAreaClear()
-            ) {
-            SpawnBall();
+        if(!gameOver)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftControl)
+                && launchArea.isLaunchAreaClear()
+                ) {
+                SpawnBall();
+            }
         }
 
     }
@@ -51,11 +66,19 @@ public class PlayerController : MonoBehaviour
             ballSpawn.transform.position,
             ballPrefab.transform.rotation
         );
-
+        if(UpdateBallCount != null)
+        {
+            UpdateBallCount(-1);
+        }
         // // calculate random torque
         // Vector3 torque = newBall.transform.right * UnityEngine.Random.Range(-maxTorque, maxTorque);
 
         // newBall.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, launchForce, 0.0f), ForceMode.Impulse);
         // newBall.GetComponent<Rigidbody>().AddTorque(torque, ForceMode.Impulse);
+    }
+
+    private void GameOver()
+    {
+        gameOver = true;
     }
 }
