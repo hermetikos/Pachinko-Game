336369ea8b762723f383f349fe8e0666774a21d0
Stan S <sslup@hermetikos.com>
2022-02-15 23:31:00 -0500

    Has all basic camera controls implemented

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float manualMoveSpeed = 3.0f;
     public float yBound = 3.0f;
     private Transform focus = null;
 
@@ -15,11 +16,12 @@ public class CameraController : MonoBehaviour
 
     void LateUpdate()
     {
-        // follow the ball
-        if(focus != null)
-        {
-            Vector3 delta = Vector3.zero;
+        Vector3 delta = Vector3.zero;
 
+        // follow the ball
+        if(focus != null
+            && !Input.GetKey(KeyCode.LeftShift))
+        {
             float dy = focus.position.y - transform.position.y;
             if(dy < -yBound || dy > yBound)
             {
@@ -32,10 +34,32 @@ public class CameraController : MonoBehaviour
                 {
                     delta.y = dy + yBound;
                 }
-            }        
-
-            transform.position += delta;
+            }
         }
+        // manual control when left shift is held
+        else if(Input.GetKey(KeyCode.LeftShift))
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                delta = Vector3.up * manualMoveSpeed;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                delta = -Vector3.up * manualMoveSpeed;
+            }
+
+            delta *= Time.deltaTime;
+        }
+        // by default, drift to the bottom of the screen
+        else
+        {
+            delta = Vector3.down * manualMoveSpeed * Time.deltaTime;
+        }
+
+        Vector3 newPosition = transform.position + delta;
+        newPosition.y = Mathf.Clamp(newPosition.y, -14.5f, 14.5f);
+
+        transform.position = newPosition;
     }
 
     public void SetFocus(GameObject ball)
